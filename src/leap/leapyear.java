d623ba04c0585894225b1402cd6f2be17dd06b5b
Parth Deshpande <parthjun27@gmail.com>
2024-08-05 23:42:33 +0530

    commit5-prog2-running unoptimised tests

--- src/leap/leapyear.java
+++ src/leap/leapyear.java
@@ -2,40 +2,58 @@ package leap;
 
 public class leapyear {
     public static void main(String[] args) {
-        int passedTests=0;
-        int failedTests=0;
-        if(testIsLeap(2024,true)){
+        int passedTests = 0;
+        int failedTests = 0;
+        if (testIsLeap(2024, true)) {
             passedTests++;
-        }else{
+        } else {
             failedTests++;
         }
-        if(testIsLeap(1900,false)){
+        if (testIsLeap(1900, false)) {
             passedTests++;
-        }else{
+        } else {
             failedTests++;
         }
-        if(testIsLeap(2004,true)){
+        if (testIsLeap(2004, true)) {
             passedTests++;
-        }else{
+        } else {
             failedTests++;
         }
-        if(testIsLeap(2021,false)){
+        if (testIsLeap(2021, false)) {
             passedTests++;
-        }else{
+        } else {
             failedTests++;
         }
+        if (testIsLeap(2000, true)) {
+            passedTests++;
+        } else {
+            failedTests++;
+        }
+        System.out.println("No of tests passed " + passedTests);
+        System.out.println("No of tests failed " + failedTests);
     }
 
     private static boolean testIsLeap(int i, boolean expectedResult) {
-        boolean result=isLeap(i);
-        if(result==expectedResult){
-            System.out.println("Test passed "+expectedResult+" got "+ result);
-        }else{
-            System.out.println("Test failed "+expectedResult+" got "+result);
+        boolean result = isLeap(i);
+        if (result == expectedResult) {
+            System.out.println("Test passed " + expectedResult + " got " + result);
+            return true;
+        } else {
+            System.out.println("Test failed " + expectedResult + " got " + result);
+            return false;
         }
     }
 
-    private static boolean isLeap(int i) {
-        throw new UnsupportedOperationException("Unimplemented method 'isLeap'");
+    private static boolean isLeap(int year) {
+        boolean ans;
+        if (year % 400 == 0) {
+            ans = true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            ans = true;
+        } else {
+            ans = false;
+        }
+        return ans;
     }
+
 }
