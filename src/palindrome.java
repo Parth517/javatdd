a023414a0851cb6c6b3e97b7a089825876770bf5
Parth Deshpande <parthjun27@gmail.com>
2024-08-05 22:50:06 +0530

    commit-1: Written tests

---
+++ src/palindrome.java
@@ -0,0 +1,25 @@
+
+
+
+public class palindrome {
+    public static void main(String[] args) {
+        int passedTests=0;
+        int failedTests=0;
+        if(testIsPalindrome("wow",true)){
+            passedTests++;
+        }else{
+            failedTests++;
+        }
+        if(testIsPalindrome("abccba",true)){
+            passedTests++;
+        }else{
+            failedTests++;
+        }
+        if(testIsPalindrome("abcdcba",false)){
+            passedTests++;
+        }else{
+            failedTests++;
+        }
+        
+    }
+}
