8e613ec057eab029de8ef45c860e6b82541041cc
Parth Deshpande <parthjun27@gmail.com>
2024-08-06 00:03:34 +0530

    prog3-commit2-wrote testcases

--- src/digittowords/digit.java
+++ src/digittowords/digit.java
@@ -2,6 +2,32 @@ package digittowords;
 
 public class digit {
     public static void main(String[] args) {
-     
+        int passedTests=0;
+        int failedTests=0;
+        if(testWords(0,"zero")){
+            passedTests++;
+        }else{
+            failedTests++;
+        }
+        if(testWords(1,"one")){
+            passedTests++;
+        }else{
+            failedTests++;
+        }
+        if(testWords(15,"fifteen")){
+            passedTests++;
+        }else{
+            failedTests++;
+        }
+        if(testWords(100,"one hundred")){
+            passedTests++;
+        }else{
+            failedTests++;
+        }
+        if(testWords(400,"four hundred")){
+            passedTests++;
+        }else{
+            failedTests++;
+        }
     }
 }
