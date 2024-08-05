e04220bdf119da8335aca258aef40787d2925636
Parth Deshpande <parthjun27@gmail.com>
2024-08-06 00:24:00 +0530

    prog3-commit4:passing all tests

--- src/digittowords/digit.java
+++ src/digittowords/digit.java
@@ -29,11 +29,18 @@ public class digit {
         }else{
             failedTests++;
         }
+        if(testWords(22,"twenty-two")){
+            passedTests++;
+        }else{
+            failedTests++;
+        }
+        System.out.println("No of test passed:"+passedTests);
+        System.out.println("No of tests failed:"+failedTests);
     }
 
     private static boolean testWords(int num, String expectedResult) {
         String result=toWords(num);
-        if(result==expectedResult){
+        if(result.equals(expectedResult)){
             System.out.println("Test passed expected "+expectedResult+" got "+result);
             return true;
         }else{
@@ -42,5 +49,17 @@ public class digit {
         }
     }
 
-    
+    private static String toWords(int num) {
+        String ans="";
+        String[] units={"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+        String[] hundreds={"","","twenty","thirty","fourty","fifty","sixty","seventy","eigty","ninety"};
+        if(num<20){
+            ans=units[(int) num];
+        }if(num>20&&num<100){
+            ans=hundreds[(int) num/10]+(num%10==0?"":"-"+toWords(num%10));
+        }if(num>=100 && num<1000){
+            ans=units[(int) num/100]+" hundred"+(num%100==0?"":" "+toWords(num%100));
+        }
+        return ans;
+    }
 }
